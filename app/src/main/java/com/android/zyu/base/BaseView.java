189ba16fe26f8489a431cc5fd2960a934098d5db
andryyu <andryyu5419@163.com>
2017-09-12 19:42:44 +0800

    first commit code

---
+++ app/src/main/java/com/android/zyu/base/BaseView.java
@@ -0,0 +1,8 @@
+package com.android.zyu.base;
+
+/**
+ * Created by yufei on 2017/9/12.
+ */
+
+public interface BaseView<T> {
+}
---
+++ app/src/main/java/com/android/zyu/data/Constants.java
@@ -0,0 +1,8 @@
+package com.android.zyu.data;
+
+/**
+ * Created by yufei on 2017/9/12.
+ */
+
+public class Constants {
+}
